dab9ed62aaf03a22362cf6496b21306876eeeeb3
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-04-15 11:55:41 +0200

    getline function

--- 3-getline.c
+++ 3-getline.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 /**
- * getline - waits for the user to enter a command, and prints it on the next line
+ * main - waits for the user to enter a command, and prints it on the next line
  * Return: number of characters read, delimiters included but excluding NULL
  * (or -1 if failure in reading process)
  */
 
-ssize_t getline(char **lineptr, size_t *n, FILE *stream)
+int main(void)
 {
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t lineread;
+       
+	printf("$ ");
 
+	lineread = getline(&line, &len, stdin);
+
+	if (lineread != -1)
+	{
+		printf("%s", line);
+		printf("%zd\n", lineread);
+	}
+	free(line);
+	return (0);
+}
