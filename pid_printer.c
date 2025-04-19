7a97e03633d632a996c9cea7cbd5fd54779606d6
root <10484@holbertonstudents.com>
2025-04-19 14:36:58 +0000

    fix: modification of the code for passing the holberton checker

--- pid_printer.c
+++ pid_printer.c
@@ -1,29 +1,25 @@
 #include "shell.h"
 
-#define MAX_ARGS 64
-
-void pid_printer(char *buf)
+void pid_printer(char **argv)
 {
     pid_t pid;
-    int status;
-    char *argv[MAX_ARGS];
-    char *token;
-    int i = 0;
-    
-    token = strtok(buf, " \t\n");
-    while (token != NULL && i < MAX_ARGS - 1)
-    {
-        argv[i++] = token;
-        token = strtok(NULL, " \t\n");
-    }
-    argv[i] = NULL;
-        pid = fork();
-        if (pid == 0)
+
+    if (access(argv[0], X_OK) == 0)
         {
-            execve(argv[0], argv, NULL);
-            perror("./shell");
-            exit(EXIT_FAILURE);
+            pid = fork();
+            if (pid == 0)
+            {
+                execve(argv[0], argv, NULL);
+                perror("./shell");
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                wait(NULL);
+            }
         }
         else
-            wait(&status);
+        {
+            perror("./shell");
+        }
 }
