f0aa64d2887e362437eb142f8b6f84f0385b9925
root <10484@holbertonstudents.com>
2025-04-19 17:21:30 +0000

    fix: trying to fix holberton checker

--- pid_printer.c
+++ pid_printer.c
@@ -3,23 +3,15 @@
 void pid_printer(char **argv)
 {
     pid_t pid;
+    int status;
 
-    if (access(argv[0], X_OK) == 0)
-        {
             pid = fork();
             if (pid == 0)
             {
                 execve(argv[0], argv, NULL);
                 perror("./shell");
-                exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
             }
             else
-            {
-                wait(NULL);
-            }
-        }
-        else
-        {
-            perror("./shell");
-        }
+                wait(&status);
 }
