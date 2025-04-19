932ed43b0ace7fb8e36c2cb333ad9fb53e928011
root <10484@holbertonstudents.com>
2025-04-19 22:56:37 +0000

    fix: add an iteration inside argv array until end of it

--- pid_printer.c
+++ pid_printer.c
@@ -1,36 +1,32 @@
 #include "shell.h"
 
-#define MAX_ARGS 64
+#define MAX 64
 
 void pid_printer(char *buf)
 {
     pid_t pid;
     int status;
-    char *argv[2];
+    char *argv[MAX];
     char *token;
     const char *delim = " \t\r\n";
     int i = 0;
     
     token = strtok(buf, delim);
-    while (token != NULL && i < MAX_ARGS - 1)
+    while (token != NULL)
     {
-	    if (access(token, X_OK) == 0)
-	    {
-	    	pid = fork();
-            	if (pid == 0)
-            	{
-                	argv[i++] = token;
+	    argv[i++] = token;
+	    token = strtok(NULL, delim);
+    }
+    argv[i] = NULL;
+   	pid = fork();
+         	if (pid == 0)
+           	{
                 	execve(argv[0], argv, NULL);
-                	perror("./shell: No such file or directory");
+                	perror("./shell: o such file or directory");
                 	exit(EXIT_FAILURE);
 		}
             	else
             	{
                 	wait(&status);
             	}
-	    }
-	    else
-		    write(STDERR_FILENO, "./shell: No such file or directory\n", 35);
-	    token = strtok(NULL, delim);
-    }
 }
