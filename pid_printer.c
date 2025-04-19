e13d4aba469c1df9150f66fb4d802bc63d354dcc
root <10484@holbertonstudents.com>
2025-04-19 18:51:29 +0000

    fix: backup version of pid_printer file

--- pid_printer.c
+++ pid_printer.c
@@ -1,17 +1,27 @@
 #include "shell.h"
 
-void pid_printer(char **argv)
+void pid_printer(char *buf)
 {
     pid_t pid;
     int status;
+    char *argv[2];
 
-            pid = fork();
-            if (pid == 0)
-            {
-                execve(argv[0], argv, NULL);
-                perror("./shell");
-		exit(EXIT_FAILURE);
-            }
-            else
-                wait(&status);
+    pid = fork();
+        if (pid < 0)
+        {
+            perror("fork");
+        }
+
+        if (pid == 0)
+        {
+            argv[0] = buf;
+            argv[1] = NULL;
+            execve(buf, argv, NULL);
+            perror("./shell");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+        }
 }
