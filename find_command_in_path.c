e3467e32a583a012d5b1b4fd870e0182ca65bed4
root <10485@holbertonstudents.com>
2025-04-20 16:51:57 +0000

    feat: add path finder function

---
+++ find_command_in_path.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+char *find_command_in_path(char *command)
+{
+    char *path_env = getenv("PATH");
+    char *path_copy = strdup(path_env);
+    char *token, *full_path = NULL;
+    struct stat st;
+
+    if (!path_env || !path_copy)
+        return NULL;
+    token = strtok(path_copy, ":");
+    while (token)
+    {
+        full_path = malloc(strlen(token) + strlen(command) + 2); // '/' + '\0'
+        if (!full_path)
+        {
+            free(path_copy);
+            return NULL;
+        }
+        sprintf(full_path, "%s/%s", token, command);
+        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+        {
+            free(path_copy);
+            return full_path; // Found!
+        }
+        free(full_path);
+        full_path = NULL;
+        token = strtok(NULL, ":");
+    }
+    free(path_copy);
+    return NULL; // Not found
+}
