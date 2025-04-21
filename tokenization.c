8d494a34dacb73ce9b5780c7a71c579212b88703
root <10485@holbertonstudents.com>
2025-04-21 14:15:39 +0000

    fix: add 1 bit to malloc new_token for accepting a pointer array in case

--- tokenization.c
+++ tokenization.c
@@ -16,7 +16,7 @@ char **tokenization(char *prompt, char *delim)
 	if (prompt == NULL || prompt[0] == '\0')
 		return (NULL);
 
-    new_token = malloc(strlen(prompt) * sizeof(char *));
+    new_token = malloc((strlen(prompt) + 1) * sizeof(char *));
 
 	token = strtok(prompt, delim);
     
