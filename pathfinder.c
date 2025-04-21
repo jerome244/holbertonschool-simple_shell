53c6af940ddd8ca44b2890a59124dfee6f53ce92
root <10485@holbertonstudents.com>
2025-04-21 10:02:23 +0000

    fix: rm j and *new unused variables

--- pathfinder.c
+++ pathfinder.c
@@ -9,18 +9,18 @@ char **pathfinder(void)
 {
         char **path_token = NULL;
         char **environment = malloc(10000);
-        char *new, *temp = NULL;
-        int i, j = 0, num = 0;
+        char *temp = NULL;
+        int i, num = 0;
         int env_count = 0;
 
         while (environ[env_count])
-    env_count++;
-    environment = malloc(sizeof(char *) * (env_count + 1));
-    if (!environment)
-        return NULL;
-    num = allocate_environment_memory(environment);
-    temp = path_finder(environment);
-    path_token = path_tokenizer(temp);
+    		env_count++;
+    	environment = malloc(sizeof(char *) * (env_count + 1));
+    	if (!environment)
+        	return NULL;
+    	num = allocate_environment_memory(environment);
+    	temp = path_finder(environment);
+    	path_token = path_tokenizer(temp);
         if (temp)
                 free(temp);
         for (i = 0; i < num; i++)
