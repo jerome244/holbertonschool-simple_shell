ba2d32ac2bb5bcec145b6f84632ec120eb215542
root <10485@holbertonstudents.com>
2025-04-21 15:08:01 +0000

    fix: integrate getenv

--- pathfinder.c
+++ pathfinder.c
@@ -1,64 +1,26 @@
 #include "shell.h"
-/**
- * pathfinder - function to locate file in the path
- * Return: array
- */
 
 char **pathfinder(void)
 {
-	char **array = NULL;
-	char **environment = malloc(10000);
-	char *new, *temp = NULL;
-	int i, j = 0, num = 0;
+    char *path_env = getenv("PATH");
+    char **array;
+    int i = 0;
 
-	while (environ[j])
-	{
-		environment[j] = malloc(strlen(environ[j]) + 1);
-		strcpy(environment[j], environ[j]);
-		j++;
-		num++;
-	}
-	j = 0;
+    if (!path_env)
+        return NULL;
 
-	while (environment[j])
-	{
-		array = tokenization(environment[j], "=");
-		if (!strcmp(*array, "PATH"))
-		{
-			if (!*(array + 1))
-				break;
-			temp = strdup(*(array + 1));
-			for (i = 0; i < 2; i++)
-				free(*(array + i));
-			free(array);
-			break;
-		}
-		for (i = 0; i < 2; i++)
-			free(*(array + i));
-		free(array);
-		j++;
-	}
-	
-	array = tokenization(temp, ":");
-	if (!array)
-		return (NULL);
-	i = 0;
-	while (array[i])
-	{
-		new = malloc(strlen(array[i]) + 2);
-		if (!new)
-			return NULL;
-		strcpy(new, array[i]);
-		strcat(new, "/");
-		free(array[i]);
-		array[i] = new;
-		i++;
-	}
+    array = tokenization(path_env, ":");
 
-	if (temp)
-		free(temp);
-	for (i = 0; i < num; i++)
-		free(environment[i]);
-	free(environment);
-	return (array);
+    while (array[i])
+    {
+        char *new = malloc(strlen(array[i]) + 2);
+        strcpy(new, array[i]);
+        strcat(new, "/");
+        free(array[i]);
+        array[i] = new;
+        i++;
+    }
+
+    return array;
 }
+
