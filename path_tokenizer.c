f1de3320b81a90df8ae492254238c1b219c8cbc1
root <10485@holbertonstudents.com>
2025-04-21 09:58:55 +0000

    feat: add function for tokenize the path string

---
+++ path_tokenizer.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+char **path_tokenizer(char *temp)
+{
+        char *new;
+        char **array = NULL;
+        int i = 0;
+
+        array = tokenization(temp, ":");
+        if (!array)
+                return (NULL);
+
+        while (array[i])
+        {
+                new = malloc(strlen(array[i]) + 2);
+                if (!new)
+                        return NULL;
+
+                strcpy(new, array[i]);
+                strcat(new, "/");
+                free(array[i]);
+                array[i] = new;
+                i++;
+        }
+
+    return (array);
+}
