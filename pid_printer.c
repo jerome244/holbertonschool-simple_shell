afc8ed8623b0553395f91dd32b2fe13992a1a5a9
root <10485@holbertonstudents.com>
2025-04-21 15:09:18 +0000

    fix: check if token[0] is NULL

--- pid_printer.c
+++ pid_printer.c
@@ -11,27 +11,34 @@ void pid_printer(char **token, char **path)
 	char *temp, *command;
 	int status, i = 0;
 
+	if (!token || !token[0])
+        	return;
+	command = strdup(token[0]);
 	pid = fork();
-		if (pid == 0)
+	if (pid == 0)
+	{
+		if (!token[0])
 		{
-			command = strdup(token[0]);
-			while (path[i])
-			{
-				execve(token[0], token, environ);
-                                temp = malloc(strlen(path[i]) + strlen(command) + 1);
-                                strcpy(temp, path[i]);
-                                strcat(temp, command);
-                                free(token[0]);
-                                token[0] = strdup(temp);
-                                free(temp);
-                                i++;
-			}
-			printf("No such file or directory\n");
-			exit(EXIT_FAILURE);
+    			perror("Empty command.\n");
+    			exit(EXIT_FAILURE);
 		}
-		else
-			wait(&status);
-		for (i = 0; *(token + i); i++)
-			free(*(token + i));
-		free(token);
+		while (path[i])
+		{
+			temp = malloc(strlen(path[i]) + strlen(command) + 1);
+                	strcpy(temp, path[i]);
+                        strcat(temp, command);
+                        free(token[0]);
+                        token[0] = strdup(temp);
+                        free(temp);
+                        execve(token[0], token, environ);
+			i++;
+		}
+		printf("No such file or directory\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+		wait(&status);
+	for (i = 0; *(token + i); i++)
+		free(*(token + i));
+	free(token);
 }
