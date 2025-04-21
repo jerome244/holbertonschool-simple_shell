384433f932b07a6905375ab540fedb1c766ee9d5
root <10485@holbertonstudents.com>
2025-04-21 11:27:41 +0000

    fix: trying to fix leak memory, rm _getline(), buf global var

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
  *
  * Return: always 0
  */
-char *buf = NULL;
+
 int main(void)
 {
-	char *prompt, **token, **path;
-	int i = 0;
+	char *prompt = NULL, **token, **path;
+	int i = 0, checker;
+	size_t prompt_size = 0;
 
 	path = pathfinder();
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			write(1, ":) ", 3);
-		prompt = _getline();
-		if (!prompt)
+		checker = getline(&prompt, &prompt_size, stdin);
+		if (checker == -1)
 			break;
 		token = tokenization(prompt, " \t\r\n");
 		free(prompt);
+		prompt = NULL;
 		
 		if (!*token)
 		{
@@ -33,10 +35,5 @@ int main(void)
 	for (i = 0; *(path + i); i++)
 		free(*(path + i));
 	free(path);
-	if (buf)
-	{
-		free(buf);
-		buf = NULL;
-	}
 	return (0);
 }
