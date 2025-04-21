7e234d4e0442bc65e582aa68cb38d5e97d830fb6
root <10485@holbertonstudents.com>
2025-04-21 15:20:14 +0000

    fix: backup

--- pathfinder.c
+++ pathfinder.c
@@ -1,26 +1,65 @@
 #include "shell.h"
 
-char **pathfinder(void)
-{
-    char *path_env = getenv("PATH");
-    char **array;
-    int i = 0;
+/**
+ * pathfinder - function to locate file in the path
+ * Return: array
+ */
 
-    if (!path_env)
-        return NULL;
-
-    array = tokenization(path_env, ":");
-
-    while (array[i])
-    {
-        char *new = malloc(strlen(array[i]) + 2);
-        strcpy(new, array[i]);
-        strcat(new, "/");
-        free(array[i]);
-        array[i] = new;
-        i++;
-    }
-
-    return array;
-}
-
+ char **pathfinder(void)
+ {
+		 char **array = NULL;
+		 char **environment = malloc(10000);
+		 char *new, *temp = NULL;
+		 int i, j = 0, num = 0;
+ 
+		 while (environ[j])
+		 {
+				 environment[j] = malloc(strlen(environ[j]) + 1);
+				 strcpy(environment[j], environ[j]);
+				 j++;
+				 num++;
+		 }
+		 j = 0;
+ 
+		 while (environment[j])
+		 {
+				 array = tokenization(environment[j], "=");
+				 if (!strcmp(*array, "PATH"))
+				 {
+						 if (!*(array + 1))
+								 break;
+						 temp = strdup(*(array + 1));
+						 for (i = 0; i < 2; i++)
+								 free(*(array + i));
+						 free(array);
+						 break;
+				 }
+				 for (i = 0; i < 2; i++)
+						 free(*(array + i));
+				 free(array);
+				 j++;
+		 }
+ 
+		 array = tokenization(temp, ":");
+		 if (!array)
+				 return (NULL);
+		 i = 0;
+		 while (array[i])
+		 {
+				 new = malloc(strlen(array[i]) + 2);
+				 if (!new)
+						 return NULL;
+				 strcpy(new, array[i]);
+				 strcat(new, "/");
+				 free(array[i]);
+				 array[i] = new;
+				 i++;
+		 }
+ 
+		 if (temp)
+				 free(temp);
+		 for (i = 0; i < num; i++)
+				 free(environment[i]);
+		 free(environment);
+		 return (array);
+ }
--- pid_printer.c
+++ pid_printer.c
@@ -5,40 +5,33 @@
  * @path: array tokenized of the path.
  */
 
-void pid_printer(char **token, char **path)
-{
-	pid_t pid;
-	char *temp, *command;
-	int status, i = 0;
-
-	if (!token || !token[0])
-        	return;
-	command = strdup(token[0]);
-	pid = fork();
-	if (pid == 0)
-	{
-		if (!token[0])
-		{
-    			perror("Empty command.\n");
-    			exit(EXIT_FAILURE);
-		}
-		while (path[i])
-		{
-			temp = malloc(strlen(path[i]) + strlen(command) + 1);
-                	strcpy(temp, path[i]);
-                        strcat(temp, command);
-                        free(token[0]);
-                        token[0] = strdup(temp);
-                        free(temp);
-                        execve(token[0], token, environ);
-			i++;
-		}
-		printf("No such file or directory\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-		wait(&status);
-	for (i = 0; *(token + i); i++)
-		free(*(token + i));
-	free(token);
-}
+ void pid_printer(char **token, char **path)
+ {
+		 pid_t pid;
+		 char *temp, *command;
+		 int status, i = 0;
+ 
+		 pid = fork();
+				 if (pid == 0)
+				 {
+						 command = strdup(token[0]);
+						 while (path[i])
+						 {
+								 execve(token[0], token, environ);
+								 temp = malloc(strlen(path[i]) + strlen(command) + 1);
+								 strcpy(temp, path[i]);
+								 strcat(temp, command);
+								 free(token[0]);
+								 token[0] = strdup(temp);
+								 free(temp);
+								 i++;
+						 }
+						 printf("No such file or directory\n");
+						 exit(EXIT_FAILURE);
+				 }
+				 else
+						 wait(&status);
+				 for (i = 0; *(token + i); i++)
+						 free(*(token + i));
+				 free(token);
+ }
