37e6832ca763e54558274d60453b5d5476d48bc1
root <10485@holbertonstudents.com>
2025-04-21 03:17:20 +0000

    feat: separate the strtok part into a new funcion for increasing the visibility of the code, and also reusing it into pathfinder function

---
+++ tokenization.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * tokenization - tokenize a string.
+ * @prompt: inputted string from user's prompt.
+ * @delim: character to search first occurence.
+ * Return: a new token.
+ */
+
+char **tokenization(char *prompt, char *delim)
+{
+	char *token;
+	char **new_token;
+	int i = 0;
+
+	if (prompt == NULL || prompt[0] == '\0')
+		return (NULL);
+
+    new_token = malloc(strlen(prompt) * sizeof(char *));
+
+	token = strtok(prompt, delim);
+    
+	while (token != NULL)
+	{
+		new_token[i] = strdup(token);
+		i++;
+		token = strtok(NULL, delim);
+	}
+	new_token[i] = NULL;
+	return (new_token);
+}
