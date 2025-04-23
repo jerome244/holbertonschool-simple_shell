3830a9d57c9028ebb94c5f5ad8eb7cb2680dcbf4
root <10485@holbertonstudents.com>
2025-04-23 08:25:14 +0000

    fix: add free_array() prototype

--- shell.h
+++ shell.h
@@ -15,5 +15,6 @@ void pid_printer(char **token, char **path);
 char **tokenization(char *prompt, char *delim);
 char *_getline(void);
 char **pathfinder(void);
+void free_array(char **array);
 
 #endif
