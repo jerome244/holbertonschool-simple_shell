c421491fd170d97547118dc478cbd919588e120b
root <10485@holbertonstudents.com>
2025-04-23 17:37:26 +0000

    fix: trying to fix

--- _setenv.c
+++ _setenv.c
@@ -4,48 +4,57 @@ extern char **environ;
 
 int _setenv(const char *name, const char *value, int overwrite)
 {
+    int i;
     size_t len;
-    char *new_var, **new_environ;
-    int i, j;
+    char *new_var;
+    char **new_environ;
 
-    if (!name || !value)
+    if (!name || !*name || !value)
         return (-1);
 
     len = strlen(name) + strlen(value) + 2;
     new_var = malloc(len);
     if (!new_var)
+	    return (-1);
 
-    sprintf(new_var, "%s=%s", name, value);
+        sprintf(new_var, "%s=%s", name, value);
 
-    for (i = 0; environ[i]; i++)
+        for (i = 0; environ[i]; i++)
     {
         if (strncmp(environ[i], name, strlen(name)) == 0 && environ[i][strlen(name)] == '=')
         {
             if (!overwrite)
             {
-                free(new_var);
-                return 0;
+                free(new_var); 
+                return (0);
             }
 
+   
             free(environ[i]);
             environ[i] = new_var;
-            return 0;
+            return (0);
         }
     }
 
-    new_environ = malloc(sizeof(char *) * (i + 2));
+   for (i = 0; environ[i]; i++) 
+        ;
+
+    new_environ = malloc(sizeof(char *) * (i + 2)); 
     if (!new_environ)
     {
         free(new_var);
-        return -1;
+        return (-1);
     }
 
-    for (j = 0; j < i; j++)
-        new_environ[j] = environ[j];
+     for (i = 0; environ[i]; i++)
+    {
+        new_environ[i] = environ[i];
+    }
     
-    new_environ[i] = new_var;
+     new_environ[i] = new_var;
     new_environ[i + 1] = NULL;
-    environ = new_environ;
+
+     environ = new_environ;
 
     return (0);
 }
--- print_env.c
+++ print_env.c
@@ -10,7 +10,7 @@
  
  	for (i = 0; environ[i]; i++)
  	{
- 		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
- 		write(STDOUT_FILENO, "\n", 1);
+ 		if (environ[i])
+			printf("%s\n", environ[i]);
  	}
  }
