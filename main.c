899d91e2f21836634317f3ec75c10d105adc7abf
root <10485@holbertonstudents.com>
2025-04-23 08:26:50 +0000

    fix: fixing leak memories issues, adding new function free_array for increasing visibility of the code

--- main.c
+++ main.c
@@ -1,42 +1,50 @@
 #include "shell.h"
 
 /**
- * main - main function
+ * main - main function of the shell
  *
  * Return: always 0
  */
-
 int main(void)
 {
-	char *prompt, **token, **path;
+	char *prompt = NULL, **token = NULL, **path = NULL;
 	int i = 0;
 
 	path = pathfinder();
+	if (!path)
+		return (1); /* Exit if path couldn't be built */
+
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(1, ":) ", 3);
+			write(STDOUT_FILENO, "$ ", 2);
+
 		prompt = _getline();
 		if (!prompt)
 			break;
+
 		token = tokenization(prompt, " \t\r\n");
 		free(prompt);
-		if (!*token)
+
+		if (!token || !*token)
 		{
-			free(token);
+			free_array(token);
 			continue;
 		}
+
 		if (strcmp(token[0], "exit") == 0)
-                {
-                        if (token[1] != NULL)
-                                _exit(2);
-                        else
-                               	_exit(0);
-                }
-        	pid_printer(token, path);
+		{
+			free_array(token);
+			free_array(path);
+			if (token[1] != NULL)
+				_exit(2);
+			else
+				_exit(0);
+		}
+
+		pid_printer(token, path);
 	}
-	for (i = 0; *(path + i); i++)
-		free(*(path + i));
-	free(path);
+
+	free_array(path);
 	return (0);
 }
