6611054ca6554b5f2c9b7db49410189b6c67510d
root <10485@holbertonstudents.com>
2025-04-23 15:47:25 +0000

    fix: tryin to fix cd

--- cd.c
+++ cd.c
@@ -1,36 +1,41 @@
 #include "shell.h"
 
 /**
- * cd - handles the 'cd' built-in command without using getenv()
+ * builtin_cd - handles the 'cd' built-in command
  * @token: tokenized user input
  * Return: 0 on success, 1 on failure
  */
+
 int cd(char **token)
 {
-	char *dir = token[1], cwd[1024];
-	char *prev_dir = _getenv_value("PWD");
+    char *dir = token[1], cwd[1024];
+    char *prev_dir;
 
-	if (!dir || strcmp(dir, "~") == 0)
-		dir = _getenv_value("HOME");
-	else if (strcmp(dir, "-") == 0)
-	{
-		dir = _getenv_value("OLDPWD");
-		if (!dir)
-			return (1);
-		write(STDOUT_FILENO, dir, strlen(dir));
-		write(STDOUT_FILENO, "\n", 1);
-	}
+    if (!dir || strcmp(dir, "~") == 0)
+        dir = getenv("HOME");
 
-	if (!dir || chdir(dir) != 0)
-	{
-		perror("cd");
-		return (1);
-	}
+    else if (strcmp(dir, "-") == 0)
+    {
+        prev_dir = getenv("OLDPWD");
+        if (!prev_dir)
+        {
+            fprintf(stderr, "cd: OLDPWD not set\n");
+            return (1);
+        }
+        printf("%s\n", prev_dir);
+        dir = prev_dir;
+    }
 
-	if (prev_dir)
-		setenv("OLDPWD", prev_dir, 1);
-	if (getcwd(cwd, sizeof(cwd)))
-		setenv("PWD", cwd, 1);
+    if (chdir(dir) != 0)
+    {
+        perror("cd");
+	return (1);
+    }
 
-	return (0);
+    setenv("OLDPWD", getenv("PWD"), 1);
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        setenv("PWD", cwd, 1);
+
+    return (0);
 }
--- main.c
+++ main.c
@@ -6,52 +6,61 @@
  */
 int main(void)
 {
- 	char *prompt = NULL, **token = NULL, **path = pathfinder();
- 	int last_status = 0;
- 
- 	if (!path)
- 		return (1);
- 
- 	while (1)
- 	{
- 		if (isatty(STDIN_FILENO))
- 			write(STDOUT_FILENO, "$ ", 2);
- 
- 		if (!(prompt = _getline()))
- 			break;
- 
- 		token = tokenization(prompt, " \t\r\n");
- 		free(prompt);
- 
- 		if (!token || !*token)
- 		{
- 			free_array(token);
- 			continue;
- 		}
- 
- 		if (!strcmp(token[0], "exit"))
- 		{
- 			free_array(token);
- 			free_array(path);
-			exit(last_status);
- 		}
+	char *prompt = NULL, **token = NULL, **commands = NULL;
+	char **path = pathfinder();
+	int last_status = 0, i;
 
- 		if (!strcmp(token[0], "env"))
- 		{
- 			print_env();
- 			free_array(token);
- 			continue;
- 		}
-		else if (!strcmp(token[0], "cd"))
-			last_status = cd(token);
-		else if (!strcmp(token[0], "setenv"))
-			last_status = builtin_setenv(token);
-		else if (!strcmp(token[0], "unsetenv"))
-			last_status = builtin_unsetenv(token);
-		else
-			last_status = program_launcher(token, path);
- 	}
+	if (!path)
+		return (1);
 
- 	free_array(path);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+
+		if (!(prompt = _getline()))
+			break;
+
+		commands = tokenization(prompt, ";");
+		if (!commands)
+		{
+			free(prompt);
+			continue;
+		}
+		free(prompt);
+
+		for (i = 0; commands && commands[i]; i++)
+		{
+			token = tokenization(commands[i], " \t\r\n");
+			if (!token || !*token)
+			{
+				free_array(token);
+				continue;
+			}
+
+			if (!strcmp(token[0], "exit"))
+			{
+				free_array(token);
+				free_array(commands);
+				free_array(path);
+				exit(last_status);
+			}
+			else if (!strcmp(token[0], "env"))
+				print_env();
+			else if (!strcmp(token[0], "cd"))
+				last_status = cd(token);
+			else if (!strcmp(token[0], "setenv"))
+				last_status = builtin_setenv(token);
+			else if (!strcmp(token[0], "unsetenv"))
+				last_status = builtin_unsetenv(token);
+			else
+				last_status = program_launcher(token, path);
+
+			free_array(token);
+		}
+		free_array(commands);
+	}
+
+	free_array(path);
 	return (0);
 }
