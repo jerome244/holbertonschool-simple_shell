d77a25adc6a5a4b16cf38a8154051d6f2ecd2cfe
root <10485@holbertstudents.com>
2025-04-23 20:22:52 +0000

    fix: trying to fix

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	char *prompt = NULL, **token = NULL, **path = pathfinder();
-   	int last_status = 0, exit_status;
+   	int last_status = 0, exit_status, i;
 
    	if (!path)
    		return (1);
@@ -34,11 +34,37 @@ int main(void)
     			exit_status = last_status;
 
     			if (token[1])
+    			{
+        			i = 0;
+
+                		if (token[1][0] == '-' || token[1][0] == '\0')
+        			{
+            				fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", token[1]);
+            				free_array(token);
+            				free_array(path);
+            				exit(2);
+        			}
+
+        			while (token[1][i])
+        			{
+            				if (token[1][i] < '0' || token[1][i] > '9')
+            				{
+                				fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", token[1]);
+                				free_array(token);
+                				free_array(path);
+                				exit(2);
+					}
+            				i++;
+        			}
+
         			exit_status = str_to_int(token[1]);
+    			}
+
     			free_array(token);
     			free_array(path);
     			exit(exit_status);
 		}
+
 
    		if (!strcmp(token[0], "env"))
    		{
